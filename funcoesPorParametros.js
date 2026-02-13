1ed39725bde71504f1c53f2ccb110a3e141261ed
arthur-henrik <arthur.henrik92@gmail.com>
2026-02-13 09:22:11 -0300

    foi

---
+++ funcoesPorParametros.js
@@ -0,0 +1,8 @@
+let resultado = soma(20, 10)
+
+function soma(numA, numB){
+    somatorio = numA + numB
+    return somatorio
+}
+
+console.log(somatorio)
---
+++ funções.js
@@ -0,0 +1,20 @@
+function paoInserido(){
+    console.log("O pão foi inserido")
+}
+
+function torrar(){
+    console.log("torando o pão")
+}
+
+function soltarPaoTorrado(){
+    console.log("O pão foi torrado e ejetado.")
+}
+
+function inserirPao(){
+    console.log("insira o pão")
+    paoInserido()
+    torrar()
+    soltarPaoTorrado()
+}
+
+inserirPao()
